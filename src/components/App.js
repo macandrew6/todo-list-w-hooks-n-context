f199a25324743fb3773bce4b4f331372fc3959bb
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-26 14:57:38 -0700

    changes basic styling

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>TodoList With Hooks and Context</h2>
+          <img
+            src="https://am24.mediaite.com/tms/cnt/uploads/2020/01/baby-yoda-big-smile-768x404.jpg"
+            alt="loading"
+            width="150px"
+            height="150px"
+            className="App-logo"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
